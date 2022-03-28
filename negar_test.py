741ee59b7ec201a01246efbac0039186d890e251
Ehsan Shams <ehsan.shams@gmail.com>
2022-03-28 01:19:12 -0700

    Update negar_test.py

--- negar_test.py
+++ negar_test.py
@@ -28,4 +28,4 @@ def write_text(texts):
     return[str(my_text)]
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8080, debug=True)
+    app.run_server(host='0.0.0.0', port=8080)
